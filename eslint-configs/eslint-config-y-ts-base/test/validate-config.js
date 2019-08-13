f3bd2dc36752635aabfa6b6c6f62e84fb6c3a28d
邵志远 <yuanhjty@163.com>
2019-08-13 23:55:17 +0800

    eslint-config-y-ts-* initial commit

---
+++ eslint-configs/eslint-config-y-ts-base/test/validate-config.js
@@ -0,0 +1,32 @@
+/* eslint-disable func-names */
+/**
+ * Modified from eslint-config-standard
+ * https://github.com/standard/eslint-config-standard/blob/master/test/validate-config.js
+ * The MIT License (MIT)
+ * Copyright (c) Feross Aboukhadijeh
+ */
+
+const path = require('path');
+const eslint = require('eslint');
+const test = require('tape');
+
+test('load config in eslint to validate all rule syntax is correct', function (t) {
+  const CLIEngine = eslint.CLIEngine;
+
+  const configFile = path.resolve(__dirname, '../index.js');
+  const cli = new CLIEngine({
+    useEslintrc: false,
+    configFile,
+  });
+  const formatter = cli.getFormatter();
+
+  const code = `
+const foo = 1;
+function bar() {}
+bar(foo);
+`;
+
+  const lintResult = cli.executeOnText(code);
+  t.equal(lintResult.errorCount, 0, formatter(lintResult.results));
+  t.end();
+});
