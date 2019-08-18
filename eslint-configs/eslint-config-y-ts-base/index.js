6fc6c5336bbe84698a32e200c5555e5e70698dab
邵志远 <yuanhjty@163.com>
2019-08-18 22:02:20 +0800

    Fix import issues when linting ts

--- eslint-configs/eslint-config-y-ts-base/index.js
+++ eslint-configs/eslint-config-y-ts-base/index.js
@@ -3,6 +3,7 @@ module.exports = {
     'y-base',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:import/typescript',
   ],
   plugins: ['@typescript-eslint'],
   parser: '@typescript-eslint/parser',
