0e913e10e0c9d903079fb729ef1ca1e9a1b138d6
邵志远 <yuanhjty@163.com>
2019-09-14 10:55:50 +0800

    chore: eslint, prettier, commintlint, git-cz, lint-staged, test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
   extends: ['./eslint-configs/eslint-config-y-base'],
-}
+  overrides: [
+    {
+      files: ['**/test/**/*.js'],
+      rules: {
+        'func-names': 'off',
+      },
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,3 @@
+module.exports = {
+  ...require('./prettier-configs/prettier-config-y-base'),
+};
